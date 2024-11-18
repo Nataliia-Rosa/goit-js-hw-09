f80f20990b09812e5bbed52ae4f3e1374b441559
Natalia <rosa.nataliya5@gmail.com>
2024-11-18 22:27:22 +0100

    finish

---
+++ src/js/2-form.js
@@ -0,0 +1,43 @@
+const STORAGE_KEY = 'feedback-form-state';
+let formData = { email: '', message: '' };
+const form = document.querySelector('.feedback-form');
+
+const saveToLocalStorage = () => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+const handleInput = event => {
+  const { name, value } = event.target;
+  formData[name] = value;
+  saveToLocalStorage();
+};
+
+const populateForm = () => {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    form.elements.email.value = formData.email || '';
+    form.elements.message.value = formData.message || '';
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if (!formData.email || !formData.message) {
+      alert('Fill please all fields');
+      return;
+    }
+
+    console.log(formData);
+
+    form.reset();
+    formData = { email: '', message: '' };
+    localStorage.removeItem(STORAGE_KEY);
+  };
+
+  form.addEventListener('input', handleInput);
+  form.addEventListener('submit', handleSubmit);
+
+  populateForm();
+};
