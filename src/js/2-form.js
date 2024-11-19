c27b9dce8d6af5a8619f594d129b81e611201118
Natalia <rosa.nataliya5@gmail.com>
2024-11-19 09:04:30 +0100

    Update 2-form.js (3)

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,45 +1,41 @@
-const STORAGE_KEY = 'feedback-form-state';
-let formData = { email: '', message: '' };
-const form = document.querySelector('.feedback-form');
+ const STORAGE_KEY = 'feedback-form-state';
+  let formData = { email: '', message: '' };
+  const form = document.querySelector('.feedback-form');
 
-const saveToLocalStorage = () => {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-};
+  const saveToLocalStorage = () => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  };
 
-const handleInput = event => {
-  const { name, value } = event.target;
-  formData[name] = value;
-  saveToLocalStorage();
-};
+  const handleInput = event => {
+    const { name, value } = event.target;
+    formData[name] = value;
+    saveToLocalStorage();
+  };
 
-const handleSubmit = event => {
-  event.preventDefault();
+  const handleSubmit = event => {
+    event.preventDefault();
 
-  if (!formData.email || !formData.message) {
-    alert('Fill please all fields');
-    return;
-  }
+    if (!formData.email || !formData.message) {
+      alert('Fill please all fields');
+      return;
+    }
 
-  console.log(formData);
+    form.reset();
+    formData = { email: '', message: '' };
+    localStorage.removeItem(STORAGE_KEY);
+  };
 
-  form.reset();
-  formData = { email: '', message: '' };
+  const populateForm = () => {
+    const savedData = localStorage.getItem(STORAGE_KEY);
 
-  localStorage.removeItem(STORAGE_KEY);
-};
+    if (savedData) {
+      formData = JSON.parse(savedData);
+      form.elements.email.value = formData.email || '';
+      form.elements.message.value = formData.message || '';
+    }
+  };
 
-const populateForm = () => {
-  const savedData = localStorage.getItem(STORAGE_KEY);
+  form.addEventListener('input', handleInput);
+  form.addEventListener('submit', handleSubmit);
 
-  if (savedData) {
-    formData = JSON.parse(savedData);
-    form.elements.email.value = formData.email || '';
-    form.elements.message.value = formData.message || '';
-  }
-};
-
-form.addEventListener('input', handleInput);
-form.addEventListener('submit', handleSubmit);
-
-populateForm();
-
+  populateForm();
